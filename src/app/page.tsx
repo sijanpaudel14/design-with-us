a3f2e0c3979e71d36c0ef79f39dc44b1ea4cb4b2
Sijan Paudel <sijanpaudel44@gmail.com>
2025-05-10 23:54:18 +0545

    fix  deplyoment errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
+import React from 'react'
 
-export default function Home() {
-  return (
-    <div>
-      Home
-    </div>
-  );
+const Home = () => {
+  return <div>Home</div>
 }
+
+export default Home
